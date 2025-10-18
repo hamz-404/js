9db0f07528c06f3e8f118403eacb2a8a1f4b9db2
hamz-404 <hamza690404@gmail.com>
2025-10-18 16:52:50 +0530

    javascript

---
+++ datatypes/stringtonocon.js
@@ -0,0 +1,22 @@
+value = 4
+sel = -value
+console.log(sel);
+
+
+let str1 = "habibi "
+let str2 = "welcome to dubai"
+let str3 = str1 + str2
+console.log(str3);
+ 
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+console.table([num1,num2,num3])
+
+let a = 4
+const b = a++ 
+console.log(`a:${a}, b:${b}`);
+
+let c = 4
+const d = ++c
+console.log(`c:${c} d:${d}`);
