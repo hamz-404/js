7a8516855688f6bc2f22ea046f49ee9415841427
hamz-404 <hamza690404@gmail.com>
2025-10-18 18:27:51 +0530

    javascript new

---
+++ datatypes/stackandheap.js
@@ -0,0 +1,24 @@
+// stack memory create a duplicate value while heap gives you a reference value 
+// for Example 
+// statck memory is of primitive datatypes
+
+let userName = "Hamza"
+let userEmail = userName
+userEmail = "Hamza@gmail.com"
+
+console.log(userName);
+console.log(userEmail);  // This is an example of Stack memory
+  
+// heap memory is of non primitive datatypes or reference datatypes
+
+let userone ={
+    email : "userone@google.com",
+    name : "userhamz"
+}
+ 
+let usertwo = userone
+userone.email = "habs@google.com"
+
+console.log(userone);
+console.log(usertwo);  // This is an example of Heap memory
+
